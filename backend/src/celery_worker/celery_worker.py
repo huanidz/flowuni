6b9f1ff60165ea37fc8b0e32d57d9243a1056bd0
Huan Nguyen <huanidz@github.com>
2025-07-21 15:23:58 +0700

    ref: update celery conf

--- backend/src/celery_worker/celery_worker.py
+++ backend/src/celery_worker/celery_worker.py
@@ -1,10 +1,8 @@
 from celery import Celery
-
 from src.configs.config import get_settings
 
 app_settings = get_settings()
 
-# Load settings from environment
 BROKER_URL = app_settings.CELERY_BROKER_URL
 RESULT_BACKEND = app_settings.CELERY_RESULT_BACKEND
 
@@ -13,6 +11,16 @@ celery_app = Celery(
     broker=BROKER_URL,
     backend=RESULT_BACKEND,
 )
+
+celery_app.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezone="UTC",
+    enable_utc=True,
+)
+
+celery_app.autodiscover_tasks(["src.celery_worker.tasks"])
 
 
 @celery_app.task
