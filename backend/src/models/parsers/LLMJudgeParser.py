f080f4047363331e6d6dcc29e6fcdb48f84355d1
HuanNT <huannt@example.com>
2025-09-24 14:13:56 +0700

    wip

--- backend/src/models/parsers/LLMJudgeParser.py
+++ backend/src/models/parsers/LLMJudgeParser.py
@@ -7,6 +7,9 @@ from src.models.parsers.LLMProviderParser import LLMProviderParser
 class LLMJudgeRuleParser(BaseModel):
     """Request schema for updating an LLM judge template"""
 
+    judge_id: Optional[int] = Field(
+        None, description="Id of the LLM judge. Same of judge template in the database"
+    )
     name: Optional[str] = Field(None, description="Name of the LLM judge template")
     description: Optional[str] = Field(
         None, description="Description of the LLM judge template"
