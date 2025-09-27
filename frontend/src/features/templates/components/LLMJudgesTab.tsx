d1a54abae6e5eb38c3e1bbd9cc8f3991ed0d4442
huanidz-hpc <huannt@personal.com>
2025-09-28 01:42:20 +0700

    wip

--- frontend/src/features/templates/components/LLMJudgesTab.tsx
+++ frontend/src/features/templates/components/LLMJudgesTab.tsx
@@ -120,7 +120,7 @@ const LLMJudgesTab: React.FC = () => {
                 </DialogContent>
             </Dialog>
 
-            {!showForm && (
+            {
                 <>
                     {data?.templates && data.templates.length > 0 ? (
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -141,7 +141,7 @@ const LLMJudgesTab: React.FC = () => {
                         </div>
                     )}
                 </>
-            )}
+            }
         </div>
     );
 };
